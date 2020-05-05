fdc904abdebc83db3e7187d4b75906c889d4db52
Kasey <kaseykaufmann@yahoo.com>
2020-05-05 19:16:34 -0400

    added personal info

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -2,9 +2,31 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <>
-      <h1>About Me</h1>
-    </>
+    <div style={{ padding: "5vw" }}>
+      <h1 style={{ paddingBottom: "20px" }}>About Me</h1>
+      <div>
+        <p>
+          My name is Kasey Kaufmann and I am a 4th year at the University of
+          Florida pursuing a bachelor’s degree in Computer Science.
+        </p>
+        <p>
+          I enjoy being challenged and engaging with projects that require me to
+          work outside my comfort and knowledge set.
+        </p>
+        <p>
+          As enclosed in my resume, I have previous experience in an internship
+          position at Horizon business services where I was responsible for
+          searching for bugs in current software, taking notes in demos of new
+          company projects, updating customer contact pages as requested using
+          Preact.
+        </p>
+        <p>
+          Currently, for academic purposes, I am learning the MERN stack and
+          working with a team in an agile based work environment to create a web
+          application for a local salon.
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -16,6 +16,10 @@ import {
 } from "@material-ui/core";
 
 import ProjectReactLogo from "../static/images/ProjectReactLogo.png";
+import ProjectARvsVR from "../static/images/ProjectARvsVR.png";
+import ProjectBlackjack from "../static/images/ProjectBlackjack.jpg";
+import ProjectMinesweeper from "../static/images/ProjectMinesweeper.png";
+import ProjectPersonalWebsite from "../static/images/ProjectPersonalWebsite.png";
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -72,43 +76,58 @@ const Projects = () => {
   );
 };
 
-const ProjectsList = [
-  {
-    language: "Javascript",
-    title: "Personal Website",
-    subheader: "Personal Project",
-    image: ProjectReactLogo,
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-    github_href: "",
-    sandbox_href:
-      "https://codesandbox.io/embed/github/kaseykaufmann/kasey-kaufmann-website/tree/master/?fontsize=14&hidenavigation=1&theme=dark",
-  },
-  {
-    language: "Javascript",
-    title: "React Project #1",
-    subheader: "Class Project",
-    image: ProjectReactLogo,
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-  },
-  {
-    language: "CPP",
-    title: "React Project #1",
-    subheader: "Class Project",
-    image: ProjectReactLogo,
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-  },
-  {
-    language: "Java",
-    title: "React Project #1",
-    subheader: "Class Project",
-    image: ProjectReactLogo,
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-  },
-];
+const ProjectsList = {
+  Javascript: [
+    {
+      title: "Personal Website",
+      subheader: "Personal Project",
+      image: ProjectPersonalWebsite,
+      content:
+        "My personal website to demonstrate who I am and the work that I have done, as well as how to reach me.",
+      github_href: "https://github.com/kaseykaufmann/kasey-kaufmann-website",
+      sandbox_href:
+        "https://codesandbox.io/embed/github/kaseykaufmann/kasey-kaufmann-website/tree/master/?fontsize=14&hidenavigation=1&theme=dark",
+    },
+    {
+      title: "VR vs AR Website",
+      subheader: "Class Project",
+      image: ProjectARvsVR,
+      content:
+        "A class project to better show the differences between what virtual reality, augmented reality and mixed reality are.",
+      github_href: "https://github.com/kaseykaufmann/VRvsARWebsite",
+      sandbox_href: "https://codepen.io/kasey-kaufmann/pen/KKdXrYy",
+    },
+    {
+      title: "NodeJS Bootcamp",
+      subheader: "Class Project",
+      image: ProjectReactLogo,
+      content:
+        "A bootcamp for a class focused on the MERN stack. It is a basic NodeJS assigment to cover the main points of the language.",
+      github_href: "https://github.com/kaseykaufmann/bootcamp_1_kasey_kaufmann",
+      //   sandbox_href: "https://codepen.io/kasey-kaufmann/pen/KKdXrYy",
+    },
+  ],
+  CPP: [
+    {
+      title: "Minesweeper",
+      subheader: "Class Project",
+      image: ProjectMinesweeper,
+      github_href: "https://github.com/kaseykaufmann/Minesweeper",
+      content:
+        "A class project using SFML and generic minesweeper pictures to implement a realistic game of Minesweeper.",
+    },
+  ],
+  Java: [
+    {
+      title: "BlackJack",
+      subheader: "Class Project",
+      image: ProjectBlackjack,
+      github_href: "https://github.com/kaseykaufmann/BlackJack",
+      content:
+        "A class project that creates an incredibly simple version of BlackJack that takes in simple user input and generates random results to simulate a simple game.",
+    },
+  ],
+};
 
 const JavascriptProjects = () => (
   <Grid
@@ -118,26 +137,26 @@ const JavascriptProjects = () => (
     alignItems="center"
     spacing={6}
   >
-    {ProjectsList.map((x, idx) =>
-      x.language === "Javascript" ? (
-        <Grid item key={idx}>
-          <Card raised style={{ width: "300px", maxHeight: "500px" }}>
-            <CardHeader title={x.title} subheader={x.subheader} />
-            <CardMedia style={{ height: "200px" }} image={x.image} />
-            <CardContent>{x.content}</CardContent>
-            <CardActions>
-              <Button target="_blank" href={x.github_href}>
-                GitHub
-              </Button>
+    {ProjectsList.Javascript.map((x, idx) => (
+      <Grid item key={idx}>
+        <Card raised style={{ width: "300px", maxHeight: "500px" }}>
+          <CardHeader title={x.title} subheader={x.subheader} />
+          <CardMedia style={{ height: "200px" }} image={x.image} />
+          <CardContent>{x.content}</CardContent>
+          <CardActions>
+            <Button target="_blank" href={x.github_href}>
+              GitHub
+            </Button>
+            {x.sandbox_href ? (
               <Button target="_blank" href={x.sandbox_href}>
                 CodePen
               </Button>
-              <Button>Live Demo</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ) : null
-    )}
+            ) : null}
+            {/* <Button>Live Demo</Button> */}
+          </CardActions>
+        </Card>
+      </Grid>
+    ))}
   </Grid>
 );
 
@@ -149,22 +168,22 @@ const CppProjects = () => (
     alignItems="center"
     spacing={6}
   >
-    {ProjectsList.map((x, idx) =>
-      x.language === "CPP" ? (
-        <Grid item key={idx}>
-          <Card raised style={{ width: "300px", maxHeight: "500px" }}>
-            <CardHeader title={x.title} subheader={x.subheader} />
-            <CardMedia style={{ height: "200px" }} image={x.image} />
-            <CardContent>{x.content}</CardContent>
-            <CardActions>
-              <Button>GitHub</Button>
-              <Button>CodePen</Button>
-              <Button>Live Demo</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ) : null
-    )}
+    {ProjectsList.CPP.map((x, idx) => (
+      <Grid item key={idx}>
+        <Card raised style={{ width: "300px", maxHeight: "500px" }}>
+          <CardHeader title={x.title} subheader={x.subheader} />
+          <CardMedia style={{ height: "200px" }} image={x.image} />
+          <CardContent>{x.content}</CardContent>
+          <CardActions>
+            <Button target="_blank" href={x.github_href}>
+              GitHub
+            </Button>
+            {/* <Button>CodePen</Button>
+              <Button>Live Demo</Button> */}
+          </CardActions>
+        </Card>
+      </Grid>
+    ))}
   </Grid>
 );
 
@@ -176,22 +195,22 @@ const JavaProjects = () => (
     alignItems="center"
     spacing={6}
   >
-    {ProjectsList.map((x, idx) =>
-      x.language === "Java" ? (
-        <Grid item key={idx}>
-          <Card raised style={{ width: "300px", maxHeight: "500px" }}>
-            <CardHeader title={x.title} subheader={x.subheader} />
-            <CardMedia style={{ height: "200px" }} image={x.image} />
-            <CardContent>{x.content}</CardContent>
-            <CardActions>
-              <Button>GitHub</Button>
-              <Button>CodePen</Button>
-              <Button>Live Demo</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ) : null
-    )}
+    {ProjectsList.Java.map((x, idx) => (
+      <Grid item key={idx}>
+        <Card raised style={{ width: "300px", maxHeight: "500px" }}>
+          <CardHeader title={x.title} subheader={x.subheader} />
+          <CardMedia style={{ height: "200px" }} image={x.image} />
+          <CardContent>{x.content}</CardContent>
+          <CardActions>
+            <Button target="_blank" href={x.github_href}>
+              GitHub
+            </Button>
+            {/* <Button>CodePen</Button>
+              <Button>Live Demo</Button> */}
+          </CardActions>
+        </Card>
+      </Grid>
+    ))}
   </Grid>
 );
 
